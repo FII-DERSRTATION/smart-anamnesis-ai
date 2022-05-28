29c384a8a598a3ec098b5027109c82027d4c8bbc
Cristian Danila <mdanila@cisco.com>
2022-05-28 03:18:03 +0300

    Add basic model setup

---
+++ main.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import pandas
+import math
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+
+
+if __name__ == '__main__':
+
+    trainX = None
+    trainY = None
+
+    # initial network
+    model = Sequential()
+    model.add(LSTM(4, input_shape=(1, look_back)))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
+
